b5ae26a327860333ebfa365406ed247ce27b3ab1
kiorky <kiorky@cryptelium.net>
2011-07-05 17:38:29 +0200

    addProducts

--- minitage/paste/projects/plone41/__init__.py
+++ minitage/paste/projects/plone41/__init__.py
@@ -81,7 +81,7 @@ for name in sources_k:
     )
 
 class Template(plone4.Template):
-    packaged_version = '4.1rc2'
+    packaged_version = '4.1rc3'
     packaged_zope2_version = '2.13.7'
     packaged_ztk_version = '1.0.2'
     summary                    = 'Template for creating a plone41 project'
